5d329d34b20e5af27b40694baa367bd6acb3713d
Frm2003 <felipper475@gmail.com>
2023-12-27 17:50:41 -0300

    first commit

---
+++ frontEnd/static/scripts/tableAdjust.js
@@ -0,0 +1,17 @@
+function load() {
+    const ths = document.querySelectorAll('th')
+    const btsTable = document.querySelectorAll('.btTable')
+
+    ths.forEach(t => {
+        let div = t.children[0]
+        if (t.getAttribute('colspan') == 2) {
+            div.style.width = `${320}px`
+        } else {
+            div.style.width = `${160}px`
+        }
+    })
+
+    btsTable.forEach(bt => {
+        bt.style.width = `${100}%`
+    })
+}
